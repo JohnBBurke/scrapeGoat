4a0fed70bb0f1eef908c32a3df650983c8490c25
macuser <tmthyjames@gmail.com>
2014-12-09 14:42:23 -0600

    add date and staffing option

--- indeed.py
+++ indeed.py
@@ -1,6 +1,8 @@
-import subprocess
+#!/usr/bin/env python
+
 from datetime import timedelta
 from os.path import expanduser
+import subprocess
 import json
 import os
 import csv
@@ -9,7 +11,17 @@ import io
 import sys
 import itertools
 
-if subprocess.call('sudo easy_install pip',shell=True) == 0:
+
+def bashScript(code,package):
+    if subprocess.call(code,shell=True) == 0:
+        print package, 'installed'
+        print '---------------------------------'
+    else:
+        subprocess.call('sudo pip install '+package, shell=True)
+        'installing ',package,'....'
+        print '---------------------------------'
+
+if subprocess.call('easy_install pip',shell=True) == 0:
     print '---------------------------------'
     print 'pip installed'
     print '---------------------------------'
@@ -19,29 +31,9 @@ else:
     print 'installing pip...'
     print '---------------------------------'
 
-if subprocess.call("python -c 'import requests'", shell=True) == 0:
-    print 'requests installed'
-    print '---------------------------------'
-else:
-    subprocess.call('sudo pip install requests==2.0.0',shell=True)
-    print 'installing requests...'
-    print '---------------------------------'
-
-if subprocess.call("python -c 'import bs4'",shell=True) == 0:
-    print 'beautifulsoup4 installed'
-    print '---------------------------------'
-else:
-    subprocess.call('sudo pip install beautifulsoup4==4.3.1',shell=True)
-    print 'installing beautifulsoup4...'
-    print '---------------------------------'
-
-if subprocess.call("python -c 'import flask'", shell=True) == 0:
-    print 'flask installed'
-    print '---------------------------------'
-else:
-    subprocess.call('sudo pip install flask==0.10.1',shell=True)
-    print 'installing flask...'
-    print '---------------------------------'
+bashScript(code="python -c 'import requests'",package='requests==2.0.0')
+bashScript(code="python -c 'import bs4'",package='beautifulsoup4==4.3.1')
+bashScript(code="python -c 'import flask'",package='flask==0.10.1')
 
 from flask import request, make_response, session
 import flask, flask.views
@@ -105,6 +97,7 @@ class View(flask.views.MethodView):
         jobType = flask.request.form['jt']
         salary = flask.request.form['salary']
         fromage = flask.request.form['fromage']
+        staffing = flask.request.form['sr']
 
         regNum = re.compile(r'[0-9]{3}-[0-9]{4}')
         altRegNum = re.compile(r'.[0-9]{3}. ?[0-9]{3}-[0-9]{4}|[0-9]{3}[-\.][0-9]{3}[-\.][0-9]{4}')
@@ -139,7 +132,7 @@ class View(flask.views.MethodView):
                 csvList.append([lead_fname,lead_mname,lead_lname,number,name_prefix,name_suffix,firmName,lead_email,lead_address,lead_apto,jobCity,jobState,lead_zip,jobTitle,lead_site,lead_title,balance_due,googleNameSearch,custom2]) 
                 with open(directory+'/'+what.upper()+where+'.csv','w') as f:
                     writer = csv.writer(f,delimiter=',',quoting=csv.QUOTE_ALL)
-                    writer.writerow(['lead_fname','lead_mname','lead_lname','lead_number','name_prefix','name_suffix','firm_name','lead_email','lead_address','lead_apto','job_city','job_state','lead_zip','job_title','lead_site','lead_title','balance_due','linkedin_data','custom2'])
+                    writer.writerow(['lead_fname','lead_mname','lead_lname','lead_number','name_prefix','name_suffix','firm_name','lead_email','lead_address','date_published','job_city','job_state','lead_zip','job_title','lead_site','lead_title','balance_due','linkedin_data','custom2'])
                     [writer.writerow(row) for row in csvList]
                 # cursor.execute('INSERT into *DB* (firmname,jobtitle,jobcity,jobstate,phoneNumber,URLtoLinkedInData) values (%s, %s, %s, %s,%s,%s)',
                 #                (firmName,jobTitle,jobCity,jobState,number,bingNameSearch))
@@ -147,7 +140,7 @@ class View(flask.views.MethodView):
         getInfo = lambda x,y,z: item.find_all(x,{y:z})[0].text.encode('utf-8').strip()
         intgr = lambda x: int(x) if x.isdigit() else x
 
-        url = 'http://www.indeed.com/search?q='+what+'&l='+where+'&sr=directhire'+'&as_any=&ttl=&jt='+jobType+'&salary='+salary+'&fromage='+fromage
+        url = 'http://www.indeed.com/search?q='+what+'&l='+where+'&sr='+staffing+'&as_any=&ttl=&jt='+jobType+'&salary='+salary+'&fromage='+fromage
         r = requests.get(url)
         soup = BeautifulSoup(r.content)
 
@@ -166,7 +159,7 @@ class View(flask.views.MethodView):
         csvList = []
         while i<searchLimit:
             try:
-                url = 'http://www.indeed.com/search?q='+what+'&l='+where+'&sr=directhire'+'&as_any=&ttl=&jt='+jobType+'&salary='+salary+'&fromage='+fromage+'&start='+str(i)
+                url = 'http://www.indeed.com/search?q='+what+'&l='+where+'&sr='+staffing+'&as_any=&ttl=&jt='+jobType+'&salary='+salary+'&fromage='+fromage+'&start='+str(i)
                 r = requests.get(url)
                 soup = BeautifulSoup(r.content)
                 gData = soup.find_all('div',{'class':'row'})
@@ -174,8 +167,9 @@ class View(flask.views.MethodView):
                     try:
                         firmInfo = [getInfo('span','class','company'),
                                     getInfo('a','target','_blank'),
-                                    getInfo('span','class','location')]
-                        firmName,jobTitle,jobCityState = firmInfo
+                                    getInfo('span','class','location'),
+                                    getInfo('span','class','date')]
+                        firmName,jobTitle,jobCityState,date = firmInfo
                         if jobCityState != 'United States':
                             jobCityState = re.sub('[0-9]*','',jobCityState).strip()
                             jobCityState = re.sub('\(.*\)','',jobCityState).strip()
@@ -203,7 +197,7 @@ class View(flask.views.MethodView):
                         #     r = requests.post(post_url, data=json.dumps(payload), headers=headers)
                         #     text = r.content
                         #     lead_site = str(json.loads(text)['id'])
-                        googleNameSearch = '"~hr" + "'+jobCityState+'"+"'+firmName+'"-intitle:"profiles" -inurl:"dir/ " site:linkedin.com/in/ OR site:linkedin.com/pub/'
+                        googleNameSearch = 'https://www.google.com/search?q="~hr" + "'+jobCityState+'"+"'+firmName+'"-intitle:"profiles" -inurl:"dir/ " site:linkedin.com/in/ OR site:linkedin.com/pub/'
                         # bingNameSearch = 'https://www.bing.com/search?q='+firmNamePlus+jobCity+'+'+jobState+'%20name%20site%3Alinkedin.com'
                         # nameReq = requests.get(bingNameSearch)
                         # nameSoup = BeautifulSoup(nameReq.content)
@@ -227,7 +221,7 @@ class View(flask.views.MethodView):
                         # lead company -> firmName
                         lead_email = 'n/a'
                         lead_address = 'n/a'
-                        lead_apto = 'n/a'
+                        lead_apto = date
                         # lead_city -> jobCity
                         # lead_state -> jobState
                         lead_zip = 'n/a'
@@ -285,49 +279,49 @@ class View(flask.views.MethodView):
         si = io.BytesIO()
         cw = csv.writer(si)
         [cw.writerow(row) for row in csvList]
-        output = make_response("lead_fname,lead_mname,lead_lname,lead_number,name_prefix,name_suffix,firm_name,lead_email,lead_address,lead_apto,job_city,job_state,lead_zip,lead_description,lead_site,lead_title,balance_due,linkedin_data,custom2"+'\n'+si.getvalue())
+        output = make_response("lead_fname,lead_mname,lead_lname,lead_number,name_prefix,name_suffix,firm_name,lead_email,lead_address,date_published,job_city,job_state,lead_zip,lead_description,lead_site,lead_title,balance_due,linkedin_data,custom2"+'\n'+si.getvalue())
         output.headers["Content-Disposition"] = "attachment; filename="+what.upper()+where+".csv"
         output.headers["Content-type"] = "text/csv"
 
-        newList = [list(x[0:2]) for x in csvList]
-        noReps = [x for x,_ in itertools.groupby(newList)]
+        # newList = [list(x[0:2]) for x in csvList]
+        # noReps = [x for x,_ in itertools.groupby(newList)]
 
-        if limit[5]>=1000:
-            scrapeRate = float(len(noReps))/len(testList)*100
-
-        else:
-            scrapeRate = float(len(noReps))/len(testList)*100
-            
-        scrapeRateStat = '%.3f'%round(scrapeRate,3)
-
-        flask.flash('\n\n')
-        flask.flash('SCRAPE RATES:')
-        stats = str(scrapeRateStat)+'% scrape rate'
-        flask.flash(stats),#'-- Actual'
-
-        testStats = '%.3f'%(float(len(noReps))/len(testList)*100)
-
-        nonScraped = []
-        newList = [i[0:2] for i in csvList]
-
-        for x in testList:
-            if x[0:2] not in newList:
-                nonScraped.append([x])
-                # with open(directory+'/NON-scraped'+what.upper()+where+'.csv','w') as f:
-                #     writer = csv.writer(f,delimiter=',',quoting=csv.QUOTE_ALL)
-                #     [writer.writerow(row) for row in nonScraped]
-                print '\n'
-            else:
-                pass
-        flask.flash('NON-scraped'+what.upper()+where+'.csv is ready in '+directory+'\n')
-        flask.flash(str(len(nonScraped))+' out of '+str(len(testList))+' were NOT scraped\n\n')
+        # if limit[5]>=1000:
+        #     scrapeRate = float(len(noReps))/len(testList)*100
 
         # else:
-        for x in testList:
-            if x[0:2] not in newList:
-                nonScraped.append([x])
-            else:
-                pass
+        #     scrapeRate = float(len(noReps))/len(testList)*100
+            
+        # scrapeRateStat = '%.3f'%round(scrapeRate,3)
+
+        # flask.flash('\n\n')
+        # flask.flash('SCRAPE RATES:')
+        # stats = str(scrapeRateStat)+'% scrape rate'
+        # flask.flash(stats),#'-- Actual'
+
+        # testStats = '%.3f'%(float(len(noReps))/len(testList)*100)
+
+        # nonScraped = []
+        # newList = [i[0:2] for i in csvList]
+
+        # for x in testList:
+        #     if x[0:2] not in newList:
+        #         nonScraped.append([x])
+        #         # with open(directory+'/NON-scraped'+what.upper()+where+'.csv','w') as f:
+        #         #     writer = csv.writer(f,delimiter=',',quoting=csv.QUOTE_ALL)
+        #         #     [writer.writerow(row) for row in nonScraped]
+        #         print '\n'
+        #     else:
+        #         pass
+        # flask.flash('NON-scraped'+what.upper()+where+'.csv is ready in '+directory+'\n')
+        # flask.flash(str(len(nonScraped))+' out of '+str(len(testList))+' were NOT scraped\n\n')
+
+        # # else:
+        # for x in testList:
+        #     if x[0:2] not in newList:
+        #         nonScraped.append([x])
+        #     else:
+        #         pass
 
         # conn.close()
         return output
